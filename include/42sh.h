c35be5eea80859c02570023c584076b951e64854
Enzo Gaggiotti <enzo.gaggiotti@epitech.eu>
2024-04-08 10:20:45 +0200

    [ADD] first commit

---
+++ include/42sh.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** Delivery
+** File description:
+** 42sh
+*/
+
+#ifndef 42SH_H_
+    #define 42SH_H_
+
+#endif /* !42SH_H_ */
---
+++ src/main.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** Delivery
+** File description:
+** main
+*/
+
+int main(int ac, char **av)
+{
+    return (0);
+}
