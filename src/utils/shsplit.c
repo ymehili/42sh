fca3fa16a2494d2d740f39b0ec2a9bc243d1acfd
ymehili <youssefmehili@gmail.com>
2024-04-24 13:15:26 +0200

    [FIX] Handle space correctly in shsplit.c

--- src/utils/shsplit.c
+++ src/utils/shsplit.c
@@ -48,8 +48,10 @@ static int handle_space(shsplit_t *shsplit, int i)
     if (is_space(shsplit->str[i]) && shsplit->quote_char == '\0') {
         shsplit->tokens[shsplit->token_index][shsplit->current_token_size]
             = '\0';
-        shsplit->token_index++;
-        shsplit->current_token_size = 0;
+        if (!is_space(shsplit->str[i + 1])){
+            shsplit->token_index++;
+            shsplit->current_token_size = 0;
+        }
         return 1;
     }
     return 0;
@@ -74,6 +76,12 @@ static void shsplit3(shsplit_t *shsplit)
     }
 }
 
+static int isspacesure(shsplit_t *shsplit, int i)
+{
+    return is_space(shsplit->str[i]) && shsplit->quote_char == '\0' &&
+        !is_space(shsplit->str[i + 1]);
+}
+
 static void process_string(shsplit_t *shsplit)
 {
     for (int i = 0; shsplit->str[i] != '\0'; i++) {
@@ -91,7 +99,7 @@ static void process_string(shsplit_t *shsplit)
                 shsplit->quote_char;
             continue;
         }
-        if (is_space(shsplit->str[i]) && shsplit->quote_char == '\0') {
+        if (isspacesure(shsplit, i)) {
             shsplit->num_tokens++;
             continue;
         }
@@ -103,16 +111,15 @@ void shsplit2(shsplit_t *shsplit)
     process_string(shsplit);
     if (shsplit->quote_char == '\0' && shsplit->current_token_size > 0)
         shsplit->num_tokens++;
-    shsplit->tokens = malloc(sizeof(char *) * (shsplit->num_tokens + 1));
+    shsplit->tokens = my_malloc(sizeof(char *) * (shsplit->num_tokens + 1));
     for (int i = 0; i < shsplit->num_tokens; i++) {
-        shsplit->tokens[i] = malloc(sizeof(char) *
-            (shsplit->current_token_size + 1));
+        shsplit->tokens[i] = my_malloc(100);
     }
 }
 
 char **shsplit(const char *str)
 {
-    shsplit_t *shsplit = malloc(sizeof(shsplit_t));
+    shsplit_t *shsplit = my_malloc(sizeof(shsplit_t));
 
     shsplit->num_tokens = 0;
     shsplit->current_token_size = 0;
